fb21370f747424c48e63487bcc0b1a4304d9bd16
Josh Thompson <thompsonjoshd@gmail.com>
2017-01-25 09:25:09 -0700

    another refactor

--- bad_connection.rb
+++ bad_connection.rb
@@ -2,18 +2,18 @@ ask_input = "What would you like to say? > "
 reply_not_upcase = "I AM HAVING A HARD TIME HEARING YOU."
 reply_all_upcase = "NO, THIS IS NOT A PET STORE"
 reply_goodbye = "ANYTHING ELSE I CAN HELP WITH?"
+exit_phrase = "GOODBYE!"
+exit_phrase_count = 0
 
-goodbyes = 0
-
-until goodbyes == 2
+while exit_phrase_count < 2
   puts ask_input
   input = gets.chomp
-  if input != input.upcase
+  if input.upcase != input
     puts reply_not_upcase
-  elsif input == input.upcase && input != "GOODBYE!"
+  elsif input.upcase == input && input != exit_phrase
     puts reply_all_upcase
-  elsif input == "GOODBYE!"
-    goodbyes += 1
+  elsif input == exit_phrase
     puts reply_goodbye
+    exit_phrase_count += 1
   end
 end
