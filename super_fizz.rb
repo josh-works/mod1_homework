3de6b128aa49ce75741e909b6c20c53f450c2cb4
Josh Thompson <thompsonjoshd@gmail.com>
2017-01-25 17:44:29 -0700

    refactor super_fiz

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,33 +1,24 @@
-# If it’s evenly divisible by 3, 5, and 7 print SuperFizzBuzz
-# If it’s evenly divisible by 3 and 7 print SuperFizz
-# If it’s evenly divisible by 5 and 7 print SuperBuzz
-# If it’s evenly divisible by 3 and 5 print FizzBuzz
-
-# If it’s evenly divisible by 3, print Fizz
-# If it’s evenly divisible by 5, print Buzz
-# If it’s evenly divisible by 7, print Super
-# Otherwise print just the number
-exit = false
-
-while exit == false
-  puts "\nWhat number do you want to test? ('exit' to exit) > "
-  input = gets.chomp
-
-  if input == "exit"
-    exit = true
-  elsif input.to_i % 3 == 0 && input.to_i % 5 == 0 && input.to_i % 7 == 0
-    puts "SuperFizzBuzz"
-  elsif input.to_i % 3 == 0 && input.to_i % 7 == 0
-    puts "SuperFizz"
-  elsif input.to_i % 5 == 0 && input.to_i % 7 == 0
-    puts "FizzBuzz"
-  elsif input.to_i % 3 == 0
-    puts "Fizz"
-  elsif input.to_i % 5 == 0
-    puts "Buzz"
-  elsif input.to_i % 7 == 0
-    puts "Super"
-  else
-    puts "your number was #{input}"
+class SuperFizz
+  def self.evaluate(number)
+      if number % 3 == 0 && number % 5 == 0 && number % 7 == 0
+        "SuperFizzBuzz"
+      elsif number % 3 == 0 && number % 7 == 0
+        "SuperFizz"
+      elsif number % 5 == 0 && number % 7 == 0
+        "FizzBuzz"
+      elsif number % 3 == 0
+        "Fizz"
+      elsif number % 5 == 0
+        "Buzz"
+      elsif number % 7 == 0
+        "Super"
+      else
+        number
+      end
   end
+end
+
+
+(1..1000).each do |number|
+  puts SuperFizz.evaluate(number)
 end
