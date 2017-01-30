f8917eef4d907cf3d5bf8bc714523b809944a736
Josh Thompson <thompsonjoshd@gmail.com>
2017-01-30 09:28:45 -0700

    broken implementaion of insertion sort
    I'm having problems keeping the enum from doing math against a nil
    value. Other than that, this works.
    
    I saw others who used the original array to add the sorted values. meh

---
+++ sorting/bubble_sort.rb
@@ -0,0 +1,17 @@
+class BubbleSort
+  def sort(input)
+    (input.length - 1).times do
+      input.map.with_index do |value, index|
+        if input[(index + 1)] == nil
+        elsif input[index] > input[(index + 1)]
+          input[index], input[(index + 1)] = input[(index + 1)], input[index]
+        end
+      end
+    end
+    p input
+  end
+end
+
+e = BubbleSort.new
+collection = [10, 15, 6, 9, 14, 4, 2, 7, 0, 1, 13, 5, 8, 3, 11, 12]
+e.sort(collection)
---
+++ sorting/insertion_sort.rb
@@ -0,0 +1,29 @@
+require 'pry'
+def insertion_sort(array)
+  sorted = []
+  (array.length - 1).times do |x|
+
+    value = array.shift
+    insertion_index = 0
+    binding.pry
+    if sorted.count == nil
+      sorted << value
+    elsif insertion_index <= sorted.length && value > sorted[insertion_index]
+      insertion_index += 1
+      sorted.insert(insertion_index,value)
+    end
+  end
+  puts sorted
+  #
+  # (1..array.length-1).each do |i|
+  #   value_to_insert = array.delete_at(i)
+  #
+  #   insertion_index = i
+  #   insertion_index -= 1 while insertion_index > 0 && value_to_insert < array[insertion_index -1]
+  #   binding.pry
+  #   array.insert(insertion_index, value_to_insert)
+  # end
+end
+
+x = [6, 8, 7, 2, 4, 0, 9, 3, 1, 5]
+insertion_sort(x)
