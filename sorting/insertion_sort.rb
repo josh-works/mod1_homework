83f84691379dae7b7f20dc8c070e4088bccb5cfc
Josh Thompson <thompsonjoshd@gmail.com>
2017-01-31 08:53:03 -0700

    this version almost works

--- sorting/insertion_sort.rb
+++ sorting/insertion_sort.rb
@@ -1,19 +1,19 @@
 require 'pry'
 def insertion_sort(array)
   sorted = []
+  sorted << array.first
   (array.length - 1).times do |x|
 
     value = array.shift
     insertion_index = 0
-    binding.pry
-    if sorted.count == nil
-      sorted << value
-    elsif insertion_index <= sorted.length && value > sorted[insertion_index]
-      insertion_index += 1
-      sorted.insert(insertion_index,value)
-    end
+    insertion_index += 1 while insertion_index <= sorted.length + 1 && value > sorted[insertion_index]
+    sorted.insert(insertion_index, value)
+    # while insertion_index <= sorted.length + 1 && value > sorted[insertion_index]
+    #   insertion_index += 1
+    #   sorted.insert(insertion_index,value)
   end
   puts sorted
+end
   #
   # (1..array.length-1).each do |i|
   #   value_to_insert = array.delete_at(i)
@@ -23,7 +23,7 @@ def insertion_sort(array)
   #   binding.pry
   #   array.insert(insertion_index, value_to_insert)
   # end
-end
+
 
 x = [6, 8, 7, 2, 4, 0, 9, 3, 1, 5]
 insertion_sort(x)
