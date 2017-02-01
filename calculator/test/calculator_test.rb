4286552130f1e7a7ae2a8ddb30d1181760097671
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-01 08:03:53 -0700

    finish calculator_test.rb

--- calculator/test/calculator_test.rb
+++ calculator/test/calculator_test.rb
@@ -1,11 +1,48 @@
-TDD Code Along
+gem 'minitest'
+require 'minitest/autorun'
+require_relative '../lib/calculator'
 
-Build a calculator class from scratch using TDD
-Start with whiteboarding and pseudocode
-Write pseudo code in the test file first for a few methods
-Your calculator should be able to handle the following methods
-.new
-#total
-#add
-#clear
-#subtract
+class CalculatorTest < Minitest::Test
+  def test_create_new_calculator
+    calculator = Calculator.new
+    assert_instance_of Calculator, calculator
+  end
+
+  def test_total_starts_empty
+    skip
+    calculator = Calculator.new
+    assert_equal 0, calculator.total
+
+  end
+
+  def test_can_add_one_number
+    calculator = Calculator.new
+    calculator.add(1)
+    assert_equal 1, calculator.total
+  end
+
+  def test_can_add_many_numbers
+    calculator = Calculator.new
+    calculator.add(10, 20, 30)
+    assert_equal 60, calculator.total
+  end
+
+  def test_can_subtract
+    calculator = Calculator.new
+    calculator.subtract(5)
+    assert_equal (-5), calculator.total
+  end
+
+  def test_can_subtract_many_numbers
+    calculator = Calculator.new
+    calculator.subtract(5, 5, 50)
+    assert_equal (-60), calculator.total
+  end
+
+  def test_can_reset_total
+    calculator = Calculator.new
+    calculator.add(10, 20, 30)
+    calculator.clear
+    assert_equal 0, calculator.total
+  end
+end
