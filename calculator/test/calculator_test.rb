72701a20989b752a74ef016c3919c0bfc9443656
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-01 07:14:28 -0700

    WIP spin up calculator homework

---
+++ calculator/test/calculator_test.rb
@@ -0,0 +1,11 @@
+TDD Code Along
+
+Build a calculator class from scratch using TDD
+Start with whiteboarding and pseudocode
+Write pseudo code in the test file first for a few methods
+Your calculator should be able to handle the following methods
+.new
+#total
+#add
+#clear
+#subtract
